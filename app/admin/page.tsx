c3cefd1bc5813e1d410179ee97cec9643203a5f9
vmc54 <vmc54@cornell.edu>
2025-04-11 14:42:21 -0400

    revert

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,18 +6,11 @@ import { todos } from "@/database/schema"
 import { Button } from "@/components/ui/button"
 import { deleteTodo } from "@/actions/todos"
 
-import { auth } from "@/lib/auth"
-
 export const dynamic = 'force-dynamic'
 
 export default async function AdminPage() {
     
     /* YOUR AUTHORIZATION CHECK HERE */
-    const { getUser } = auth()
-    const user = await getUser()
-    if (!user) {
-        return null
-    }
 
     const allTodos = await db.query.todos.findMany({
         with: {
